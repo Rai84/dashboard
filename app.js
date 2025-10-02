d7ea3885e7fddfa4c05dafd06f3f9e9fa175c26f
Rai <rai.mgoncalves@senacsp.edu.br>
2025-10-02 14:57:47 -0300

    0.6

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const { createClient } = supabase;
-
-// 🔑 Substitua pelas suas credenciais do Supabase
-const SUPABASE_URL = "https://fprppmxfxwrswlhsxeww.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwcnBwbXhmeHdyc3dsaHN4ZXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzQ0OTMsImV4cCI6MjA3NDk1MDQ5M30.Wv918OkNZoFL6lAVkxX-IAxzibyA6qqnqFjZGqb0kSI";
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
-
 const formAdd = document.getElementById("formAdd");
 const inputNome = document.getElementById("nome");
 const selectTipo = document.getElementById("tipo");
